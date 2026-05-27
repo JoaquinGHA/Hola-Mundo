2ba4ac2b3803d8842d3178342c7f55c25269d525
Joaquin <joaquingabriel10@gmail.com>
2026-05-27 14:15:35 +0200

    Primer commit - Hola Mundo

---
+++ src/HolaMundo/HolaMundo.java
@@ -0,0 +1,19 @@
+package HolaMundo;
+
+public class HolaMundo {
+	
+	public  String getHolaMundo() {
+		return "Hola Mundo";
+	}
+	public  String getPruebaGit() {
+		return "Esto debe estar en GitHub";
+	}
+	public  String getEstoGit() {
+		return "Esto también debe aparecer";
+	}
+	
+	
+	
+	
+
+}
---
+++ src/Main/Main.java
@@ -0,0 +1,14 @@
+package Main;
+import HolaMundo.HolaMundo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        HolaMundo hm = new HolaMundo();
+        System.out.println(hm.getHolaMundo());
+        System.out.println(hm.getPruebaGit());
+        System.out.println(hm.getEstoGit());
+        
+    }
+}
