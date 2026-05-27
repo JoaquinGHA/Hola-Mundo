7bd747d924fa239e8df6143c21f9c9c4f3b1ce5e
Joaquin <joaquingabriel10@gmail.com>
2026-05-27 15:26:48 +0200

    Agrega un método en Hola Mundo y en Main

--- src/HolaMundo/HolaMundo.java
+++ src/HolaMundo/HolaMundo.java
@@ -2,15 +2,10 @@ package HolaMundo;
 
 public class HolaMundo {
 	
-	public  String getHolaMundo() {
-		return "Hola Mundo";
-	}
-	public  String getPruebaGit() {
-		return "Esto debe estar en GitHub";
-	}
-	public  String getEstoGit() {
-		return "Esto también debe aparecer";
-	}
+	public String getHolaMundo() {return "Hola Mundo";}
+	public String getPruebaGit() {return "Esto debe estar en GitHub";}
+	public String getEstoGit() {return "Esto también debe aparecer";}
+	public String getActualización1() {return "Esto es que se ha actualizado";}
 	
 	
 	
